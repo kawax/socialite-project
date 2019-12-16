65d8c9e3d164656b6c441920b930a06fabb2868c
kawax <kawaxbiz@gmail.com>
2019-12-16 16:53:06 +0900

    cu

---
+++ README.md
@@ -0,0 +1,5 @@
+# Socialite Demo
+
+```
+php artisan serve 
+```
--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Socialite;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
